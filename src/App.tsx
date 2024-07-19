540f6436b217b3f210981a2e6b9dd993700b64d2
Jason <code@jasonallan.dev>
2024-07-19 13:38:38 +0100

    Make it mine!

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,87 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
+import { SquareArrowOutUpRight } from "lucide-react"
+import quokka from "../public/quokka.png"
+import {
+	SiGithub,
+	SiMastodon,
+	SiStackoverflow,
+} from "@icons-pack/react-simple-icons"
 
 function App() {
-	const [count, setCount] = useState(0)
+	return (
+		<div className="w-full h-full min-w-80">
+			<div
+				className="
+					flex flex-col gap-4 items-center justify-end
+					p-4
+					min-h-[50svh]
+				"
+				data-theme="light"
+			>
+				<Top />
+			</div>
+			<div
+				className="
+					flex flex-col gap-8 items-center
+					p-8
+					min-h-[50svh]
+				"
+				data-theme="dark"
+			>
+				<Bottom />
+			</div>
+		</div>
+	)
+}
 
+function Top() {
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
+			<div className="
+				border-solid border-neutral border-8 rounded-full
+				w-fit
+			">
 					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
+					className="
+						w-64 max-md:w-32
+						border-solid border-white border-[12px] rounded-full
+					"
+						src={quokka}
+						alt="A happy cartoon quokka used as an avatar"
 					/>
+				</div>
+
+			<h1 className="
+				uppercase font-extrabold
+				text-5xl max-md:text-3xl
+				text-center
+			">
+					Jason Allan
+				</h1>
+			<h2 className="text-4xl max-md:text-2xl font-bold text-center">
+				Web Expert
+			</h2>
+		</>
+	)
+}
+
+function Bottom() {
+	return (
+		<>
+			<div className="
+				text-xl max-md:text-base
+				font-extrabold
+				text-center
+			">
+				Your friendly internet developer
+			</div>
+				<a
+					href="https://cal.com/oiyouyeahyou/consultation"
+				className="btn btn-primary h-auto min-h-12"
+					target="_blank"
+				>
+					<span>Schedule A Call</span>
+					<SquareArrowOutUpRight />
 				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
 		</>
 	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,45 @@
+const daisyThemes = require("daisyui/src/theming/themes")
+
+const globalOverrrides = {
+	"--rounded-btn": "0em",
+	".btn": {
+		paddingLeft: "2em",
+		paddingRight: "2em",
+	},
+}
+
+function customTheme(theme, extra) {
+	if (!Object.hasOwn(daisyThemes, theme)) {
+		throw new TypeError(`Daisy UI doesn't have this theme: ${theme}`)
+	}
+
+	return {
+		...daisyThemes[theme],
+		...globalOverrrides,
+		...extra,
+	}
+}
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				fira: "Yeseva One, Times New Roman, serif",
+			},
+		},
 	},
-	plugins: [],
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				light: customTheme("light"),
+				dark: customTheme("dark", {
+					primary: "white",
+					"base-100": "black",
+				}),
+			},
+		],
+	},
 }
